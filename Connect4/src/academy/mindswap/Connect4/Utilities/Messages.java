5bd24787fb4012c12a95ecb1a205bc7d355ebad1
Luís Ribeiro <luispedro.ribeiro90@gmail.com>
2022-08-22 10:03:51 +0100

    added messages in server and player classes...

--- Connect4/src/academy/mindswap/Connect4/Utilities/Messages.java
+++ Connect4/src/academy/mindswap/Connect4/Utilities/Messages.java
@@ -3,7 +3,7 @@ package academy.mindswap.Connect4.Utilities;
 public class Messages {
 
     public final static String YOUR_TURN = "It's your turn, please inset move from 0 to 6";
-    public final static String OPPONENT_TURN = "It's your opponent's turn.";
+    public final static String OPPONENT_TURN = "It's your opponent's turn!";
     public final static String WANT_TO_PLAY = "Want to play another game?";
     public final static String NEED_A_PLAYER = "We need one more player, please wait.";
     public final static String GOODBYE = "See you tomorrow!";
